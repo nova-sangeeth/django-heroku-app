e665b90ab9b633079de12a216b4b3676253f84c2
nova-sangeeth <novasangeeth@outlook.com>
2020-05-28 19:12:00 +0530

    Added heroku configs to the settings.py file

--- profile_management/settings.py
+++ profile_management/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'profile_management.urls'
@@ -138,4 +140,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
+
+if os.getcwd() == '/app:':
+    db_from_env = dj_database_url.config(con_max_age=500)
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PHOTO', 'https')
+    BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DEBUG  = True
+    
+
+
